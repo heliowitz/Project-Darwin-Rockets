0fd3a927c4f1154a1b578989aea8f173a76f5226
jack.ding <jackding.email@gmail.com>
2017-01-12 23:30:27 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function setup() {
+	createCanvas(600,400);
+}
+function draw(){
+	background(0);
+}
+function Rocket(){
+	this.pos=createVector();
+	this.vel=createVector();
+	this.acc=createVector();
+
+	this.applyForce = function(force){
+		this.acc.add(force);
+	}
+
+	this.update = function(){
+		this.vel.add(this.acc);
+		this.pos.add(this.vel);
+		this.acc.mult(0);
+	}
+
+	this.show = function(){
+		translate(this.pos.x, this.pos.y);
+		rotate(this.vel.heading());
+		rectMode(CENTER);
+		rect(0,0,10,50);
+	}
+}
