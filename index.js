49d8a71a61fb67af8ec8747ec26e7ed5d9213d8a
jack.ding <jackding.email@gmail.com>
2017-01-13 00:27:41 -0500

    fitness function as manhattan distance

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var population;
 var lifespan = 200;
 var lifeP;
 var count = 0;
+var target;
 
 function setup() {
-	createCanvas(600,400);
+	createCanvas(900,600);
 	rocket = new Rocket();
 	population = new Population();
 	lifeP = createP();
+	target=createVector(width/2, 50);
 }
 
 function draw(){
@@ -16,14 +18,47 @@ function draw(){
 	population.run();
 	lifeP.html(count);
 	count++;
+
+	if (count == lifespan){
+		count=0;
+	}
+
+	ellipse(target.x, target.y, 16,16);
 }
 
 function Population(){
 	this.rockets=[];
 	this.popsize = 25;
+	this.matingPool = [];
 
 	for (var i = 0; i <this.popsize; i++){
 		this.rockets[i] = new Rocket();
+	}
+
+	this.evaluate = function(){
+		var maxfit = 0;
+		for (var i = 0; i < this.popsize; i++){
+			this.rocket[i].calcFitness();
+			if (this.rockets[i].fitness > maxfit){
+				maxfit = this.rockets[i].fitness;
+			}
+		}
+
+		for (var i = 0; i < this.popsize; i++){
+			this.rockets[i].fitness /= maxfit;
+		}
+
+		this.matingPool = [];
+		for (var i = 0; i < this.popsize; i++){
+			var n = this.rockets[i].fitness*100;
+			for (var j = 0; j<n; j++){
+				this.matingpool.add(this.rockets[i]);
+			}
+		}
+	}
+
+	this.selection = function(){
+		
 	}
 
 	this.run = function(){
@@ -47,6 +82,12 @@ function Rocket(){
 	this.vel=createVector();
 	this.acc=createVector();
 	this.dna = new DNA();
+	this.fitness;
+
+	this.calcFitness = function(){
+		var d = dist(this.pos.x, this.pos.y, target.x, target.y);
+		this.fitness = map(d,0,width, width,0);
+	}
 
 	this.applyForce = function(force){
 		this.acc.add(force);
